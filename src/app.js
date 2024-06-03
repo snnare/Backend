6b5b3a37a80a5d0c973b750b5c6af12863458446
Jose Rios <roraj77@hotmail.com>
2024-06-03 13:41:51 -0400

    testeo cors con ips publicas

--- src/app.js
+++ src/app.js
@@ -10,11 +10,25 @@ import cors from 'cors'
 const FRONT_URL = "http://[2806:2f0:9021:cb34:41b3:d385:5246:d5b9]:8081";
 
 
+// URLs del frontend
+const ALLOWED_ORIGINS = [
+    "http://[2806:2f0:9021:cb34:41b3:d385:5246:d5b9]:8081", // IPv6
+    "http://192.168.100.82:8081", // IPv4 local
+    "http://187.190.193.53:8081",// IPv4 pública 
+]
+
+
 const  app = express()
 
 app.use(cors({
     credentials: true,
-    origin: FRONT_URL
+    origin: (origin, callback) => {
+        if(ALLOWED_ORIGINS.includes(origin) || !origin){
+            callback(null, true)
+        } else{
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
 }))
 app.use(morgan('dev'))
 app.use(express.json())
